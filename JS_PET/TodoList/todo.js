d501b0d9b2621c3abd89947d89b4b4fc821a8243
DangLinh92 <hoamoclan.3192@gmail.com>
2020-07-05 21:16:27 +0700

    todo project

--- JS_PET/TodoList/todo.js
+++ JS_PET/TodoList/todo.js
@@ -1,23 +1,27 @@
 var btn = document.getElementById("task_header_btnAdd");
-btn.addEventListener("click",function(){
-            var taskName = document.getElementById("task_header_input").value;
-            if(taskName){
-                var taskItem = document.createElement("li");
-                taskItem.setAttribute("class","task_item");
-                var inputChecked = document.createElement("input");
-                inputChecked.setAttribute("type","checkbox");
-                inputChecked.setAttribute("class","task_item--checked");
-                var taskItemName = document.createElement("span");
-                taskItemName.setAttribute("class","task_item_name");
-                taskItemName.textContent = taskName;
-                var iconDelete = document.createElement("i");
-                iconDelete.setAttribute("class", "fas fa-trash-alt task_item--delete");
-        
-                taskItem.appendChild(inputChecked);
-                taskItem.appendChild(taskItemName);
-                taskItem.appendChild(iconDelete);
-        
-                var tasks = document.getElementById("tasks");
-                tasks.appendChild(taskItem);
-            }
+btn.addEventListener("click", function() {
+    var taskName = document.getElementById("task_header_input").value;
+    if (taskName) {
+        var taskItem = document.createElement("li");
+        taskItem.setAttribute("class", "task_item");
+        var inputChecked = document.createElement("input");
+        inputChecked.setAttribute("type", "checkbox");
+        inputChecked.setAttribute("class", "task_item--checked");
+        var taskItemName = document.createElement("span");
+        taskItemName.setAttribute("class", "task_item_name");
+        taskItemName.textContent = taskName;
+        var iconDelete = document.createElement("i");
+        iconDelete.setAttribute("class", "fas fa-trash-alt task_item--delete");
+
+        iconDelete.addEventListener("click", function(ev) {
+            tasks.removeChild(this.parentElement);
         });
+
+        taskItem.appendChild(inputChecked);
+        taskItem.appendChild(taskItemName);
+        taskItem.appendChild(iconDelete);
+
+        var tasks = document.getElementById("tasks");
+        tasks.appendChild(taskItem);
+    }
+});
