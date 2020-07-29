59c8874b36d5563317211ae6acae14be6c1704e0
DangLinh92 <hoamoclan.3192@gmail.com>
2020-07-29 23:05:40 +0700

    update controller

--- ExpressJs/BaiTap/books/routers/user.router.js
+++ ExpressJs/BaiTap/books/routers/user.router.js
@@ -4,15 +4,10 @@ const router = express.Router();
 const controller = require('../controllers/user.controller')
 
 router.get('/', controller.index);
-
 router.get('/new', controller.new);
-
 router.post('/new', controller.postNew);
-
 router.get('/edit/:id', controller.edit);
-
 router.post('/edit', controller.postEdit);
-
 router.get('/delete/:id', controller.delete);
 
 module.exports = router;
