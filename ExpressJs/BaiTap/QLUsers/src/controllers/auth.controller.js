0dee2cafa8b9d52ba6bfd556b807049e695c6259
DangLinh92 <hoamoclan.3192@gmail.com>
2020-08-10 22:15:47 +0700

    add bcrypt and max retry input password

--- ExpressJs/BaiTap/QLUsers/src/controllers/auth.controller.js
+++ ExpressJs/BaiTap/QLUsers/src/controllers/auth.controller.js
@@ -1,21 +1,35 @@
 const db = require('../lowdb');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports.getLogin = (req, res) => {
     res.clearCookie('userId');
     return res.redirect('login');
 };
 
+var RetryCount = 0;
 module.exports.postLogin = (req, res) => {
-    var user = req.body;
-    var userDb = db
-        .get('users')
-        .find({ email: user.email, password: user.password })
-        .value();
-    if (userDb) {
-        res.cookie('userId', userDb.id, { maxAge: 900000, signed: true });
-        res.locals.userName = userDb.name;
-        return res.render('home', { users: db.get('users').value() });
+    console.log(RetryCount);
+    RetryCount += 1;
+    if (RetryCount > 5) {
+        return res.render('login', {
+            messageErr: 'Số lần nhập vượt quá 5 lần',
+        });
     }
-
-    return res.redirect('login');
+    var user = req.body;
+    var userDb = db.get('users').find({ email: user.email }).value();
+    if (userDb) {
+        bcrypt.compare(user.password, userDb.password, function (err, result) {
+            if (result) {
+                res.cookie('userId', userDb.id, {
+                    maxAge: 900000,
+                    signed: true,
+                });
+                res.locals.userName = userDb.name;
+                RetryCount = 0;
+                return res.render('home', { users: db.get('users').value() });
+            }
+        });
+    }
+    return res.render('login');
 };
--- ExpressJs/BaiTap/QLUsers/src/controllers/user.controller.js
+++ ExpressJs/BaiTap/QLUsers/src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const shortid = require('shortid');
 const db = require('../lowdb');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports.newUser = (req, res) => {
     return res.render('newUser');
@@ -11,16 +13,22 @@ module.exports.postNew = (req, res) => {
     var order = db.get('users').findLast().value().order;
     if (!isNaN(order)) {
         var odr = Number.parseInt(order) + 1;
-        db.get('users')
-            .push({
-                order: odr,
-                id: userId,
-                name: user.name,
-                email: user.email,
-                password: user.password,
-                phone: user.phone,
-            })
-            .write();
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                // Store hash in your password DB.
+                db.get('users')
+                    .push({
+                        order: odr,
+                        id: userId,
+                        name: user.name,
+                        email: user.email,
+                        password: hash,
+                        phone: user.phone,
+                    })
+                    .write();
+
+                return res.redirect('/users/list');
+            });
+        });
     }
-    return res.redirect('/users/list');
 };
